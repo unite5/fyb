f835980f435a2562c02a6e37f6390ab04d03b258
jitendrahpatwa <jitendrapatwa135@gmail.com>
2016-12-23 00:13:21 +0530

    initial

--- www/js/controller/commentsController.js
+++ www/js/controller/commentsController.js
@@ -5,13 +5,13 @@ angular.module('besties')
         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
         
         var disqus_config = function () {
-        this.page.url = 'http://192.168.1.104:8100/#/app/comments';  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.url = '/#/app/comments';  // Replace PAGE_URL with your page's canonical URL variable
         this.page.identifier = 'besties'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
         
         (function() { // DON'T EDIT BELOW THIS LINE
         var d = document, s = d.createElement('script');
-        s.src = '//jpbesties.disqus.com/embed.js';
+        s.src = 'http://jpbesties.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
         })();
