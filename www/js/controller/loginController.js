85ed0cd707775b1a82ab93c8cd21844a85af5cdc
jitendrahpatwa <jitendrapatwa135@gmail.com>
2016-12-22 23:32:41 +0530

    initial

--- www/js/controller/loginController.js
+++ www/js/controller/loginController.js
@@ -1,7 +1,20 @@
 angular.module('besties')
-.controller('loginController',function($scope,$log,$state){
+.controller('loginController',function($scope,$log,$state,$timeout){
 	$log.warn('in loginController');
 	$scope.home = function(){
 		$state.go('app.home');
-	}
+	};
+
+	$scope.staydelay = function(){
+		$timeout(function(){
+			alert('going via a ');
+			location.href="/#/app/home";
+		},2000);
+	};
+	$scope.staydelay = function(){
+		$timeout(function(){
+			alert('going via go ');
+			$state.go('app.home');
+		},2000);
+	};
 })
