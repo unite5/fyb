f225737f279ae233799336821ca8f72a26c3f739
jitendrahpatwa <jitendrapatwa135@gmail.com>
2017-01-26 22:30:21 +0530

    initial

--- www/js/controller/loginController.js
+++ www/js/controller/loginController.js
@@ -2,12 +2,36 @@
 besties.controller('loginController',
 	//['$scope',"$ionicPopup","$log","$state","$timeout","$ionicLoading","meloginfact","$http",
 	function($scope,$ionicPopup,$log,$state,$timeout,$ionicLoading,meloginfact,$http){
-	  alert("inn;loginController");
+	  //alert("inn;loginController");
+	  document.getElementById("btngo2").style.display = "none";
+	  $scope.btngo2 = true;
+	  document.getElementById("btngo3").style.display = "none";
+	  $scope.btngo3 = true;
+	  document.getElementById("btngo4").style.display = "none";
+	  $scope.btngo4 = true;
+	  document.getElementById("seconddivradiodiv").style.display = "none";
+	  $scope.seconddivradiodiv = true;
+	  
 	  $scope.open1 = false;
 	  $scope.open2 = true;
 	  $scope.open3 = true;
 	  $scope.open4 = true;
 	  $scope.open5 = true;
+	  /*$scope.open1 = true;
+	  $scope.open2 = true;
+	  $scope.open3 = true;
+	  $scope.open4 = false;
+	  $scope.open5 = true;*/
+	  $scope.formdata = {
+		'uname':'',
+		'gender':'',
+		'uphone':'',
+		'otp':''
+	  }
+	  $timeout(function(){
+		$scope.open1 = true;//div hide
+	    $scope.open2 = false;
+	  },4000);
 	  $scope.call = function(){
 	    // if ng-hide false then visible
 	    // if ng-hide true then invisible
@@ -20,14 +44,30 @@ besties.controller('loginController',
 	  $scope.call2 = function(){
 	    $scope.open3 = false;//div show
 	    $scope.open2 = true;//div hide
+	    var phone =angular.element(document.getElementById("txtPhone")).val();//document.getElementById("thirddivinput").value;
+		console.log(phone);
+		var lat = 19.235234, lon = 73.1275884;
+		var datas = {
+			phone:phone,
+			lat:lat,
+			lon:lon,
+			deviceid:'12332434'
+		};//lat lon deviceid
+		//meloginfact.registerforOtp(phone,$scope,datas,thirddiv,otpdiv,$http,$timeout,$ionicPopup,$ionicLoading);
 	  }
 	  $scope.call3 = function(){
 	    $scope.open4 = false;//div show
 	    $scope.open3 = true;//div hide
+	    var otptxtdivinput = angular.element(document.getElementById("txtOTP")).val();//$scope.formdata.otp;
+	    console.log(otptxtdivinput);
+		//meloginfact.callotp(otpdiv,seconddiv,forthdiv,$scope,$timeout,$ionicLoading,$http,$ionicPopup,otptxtdivinput);
 	  }
 	  $scope.call4 = function(){
 	    $scope.open5 = false;//div show
 	    $scope.open4 = true;//div hide
+	    console.log($scope.formdata.uname);
+	    console.log($scope.formdata.gender);
+	    //meloginfact.addmyDetails(seconddiv,forthdiv,$scope,$http,$ionicPopup,$timeout,$ionicLoading);
 	  }
 	  $scope.call5 = function(){
 	    $scope.open5 = true;//div hide
@@ -35,6 +75,10 @@ besties.controller('loginController',
 	    //$scope.open1 = false;//div show
 	    location.href="index.html";
 	    localStorage.imin = "Y";
+	  }
+	  $scope.callsubmitdivsecond = function(){
+		btngo4.style.display = "block";
+		$scope.btngo4 = false;
 	  }
 
 	//scope.regex = '\\d+[0-9]{10}';///^[0-9]+$/
@@ -167,11 +211,14 @@ besties.controller('loginController',
                     if(text.length<3){
                         //console.log("less than 3");
                         seconddivradiodiv.style.display = "none";
-                        document.getElementById("btngo1").style.display = "none";
+                        scope.seconddivradiodiv = true;
+                        /*document.getElementById("btngo4").style.display = "none";
+                        scope.btngo4 = true;*/
                     }
                     if(text.length>3){
                         //console.log("greater than 3");
                         seconddivradiodiv.style.display = "block";
+                        scope.seconddivradiodiv = false;
                     }
                     //console.log(text+" less than 3 "+text.length+" "+transformedInput);
                     return transformedInput;
@@ -203,10 +250,12 @@ besties.controller('loginController',
                     	if(text.length<10){
 	                    	btngo2.style.display = "none";
 	                        //console.log("less than 10");
+	                        scope.btngo2 = true;
 	                    }
 	                    if(text.length == 10){
 	                    	btngo2.style.display = "block";
 	                        //console.log("equals 10");
+	                        scope.btngo2 = false;
 	                    }
 	                    if(text.length > 10){
 	                    	/*var d = document.getElementById('thirddivinput').value;
@@ -215,10 +264,12 @@ besties.controller('loginController',
 	                    	console.log("after val is:"+d.substr(0, d.length - 1));*/
 	                    	btngo2.style.display = "none";
 	                        //console.log("more than 10");
+	                        scope.btngo2 = true;
 	                    }
 	                    return transformedInput;
                     }else{
-                    	thirddivinput.val("");
+                    	//thirddivinput.val("");
+                    	scope.btngo2 = true;
                     	return false;
                     }
                     
@@ -244,14 +295,17 @@ besties.controller('loginController',
                         ngModelCtrl.$render();
                     }
                     if(text.length<6){
+                    	scope.btngo3 = true;
                     	btngo3.style.display = "none";
                         //console.log("less than 6");
                     }
                     if(text.length == 6){
+                    	scope.btngo3 = false;
                     	btngo3.style.display = "block";
                         //console.log("equals 6 and validateOtp");
                     }
                     if(text.length > 6){
+                    	scope.btngo3 = true;
                     	btngo3.style.display = "none";
                         //console.log("more than 6");
                     }
