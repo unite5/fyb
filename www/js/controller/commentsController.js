f6a298ac2e31197bc5906129fbe9505b433e1ffa
jitendrahpatwa <jitendrapatwa135@gmail.com>
2017-01-30 00:42:46 +0530

    initial

--- www/js/controller/commentsController.js
+++ www/js/controller/commentsController.js
@@ -5,7 +5,7 @@ besties.controller('commentsController',function($scope){
         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
         
         var disqus_config = function () {
-        this.page.url = '/#/app/comments';  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.url = 'http://test.dr-ambedkar.in/mobile/app/comments';  // Replace PAGE_URL with your page's canonical URL variable
         this.page.identifier = 'besties'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
         
