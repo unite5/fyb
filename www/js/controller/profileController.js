667b9a89421f1a1e472850fb733b3fb6d28c4261
Jitendra Patwa <jitendrapatwa135@gmail.com>
2017-03-09 23:15:39 +0530

    first

--- www/js/controller/profileController.js
+++ www/js/controller/profileController.js
@@ -1,6 +1,9 @@
 //angular.module('besties')
-besties.controller('profileController',function($scope,$cordovaSQLite,$ionicLoading,$ionicPopup,$http,profileservice,$timeout,$cordovaToast){
+besties.controller('profileController',function($scope,$cordovaSQLite,$ionicLoading,$ionicPopup,$http,profileservice,$timeout,$cordovaToast,$cordovaCamera, $cordovaCapture){
 	
+	$scope.my = {
+		'image':''
+	};
 	$scope.profilemodel = {
 		'email':'',
 		'age':'',
@@ -18,4 +21,65 @@ besties.controller('profileController',function($scope,$cordovaSQLite,$ionicLoad
 		console.log($scope.profilemodel);
 		profileservice.sendToMyProfile($http,$cordovaSQLite,$scope,$ionicLoading,$ionicPopup,$timeout,$cordovaToast);
 	};
+
+	$scope.chooseOptionToChangePic = function(){
+		var con = '<div class="row"><div class="col col-50"><button class="button button-large button-positive" ng-click="gallery()">  Gallery </button></div>'+
+				   '<div class="col col-50"><button class="button button-large button-positive"  ng-click="camera()"> Camera </button></div></div>';
+		var meetPopup = $ionicPopup.alert({
+			title:'Choose From',
+			cssClass:'profileChoosePopup',
+			content:con,
+			scope:$scope
+		}).then(function(){
+			setTimeout(function(){
+				meetPopup.close();
+			},3000);
+			console.log("done");
+		});
+	};
+
+	$scope.gallery = function(){
+		console.log("gallery");
+		var options = {
+	          quality: 75,
+	          destinationType: Camera.DestinationType.DATA_URL,
+	          sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+	          allowEdit: true,
+	          encodingType: Camera.EncodingType.JPEG,
+	          targetWidth: 300,
+	          targetHeight: 300,
+	          popoverOptions: CameraPopoverOptions,
+	          saveToPhotoAlbum: false
+	      };
+
+	      $cordovaCamera.getPicture(options).then(function (imageData) {
+	          $scope.pic = "data:image/png;base64," + imageData;
+	          $scope.my.image = "data:image/png;base64," + imageData;
+	      }, function (err) {
+	          alert('can not load image');
+	      });
+	}
+
+	$scope.camera = function(){
+		console.log("camera");
+		var options = {
+	          quality: 75,
+	          destinationType: Camera.DestinationType.DATA_URL,
+	          sourceType: Camera.PictureSourceType.CAMERA,
+	          allowEdit: true,
+	          encodingType: Camera.EncodingType.JPEG,
+	          targetWidth: 300,
+	          targetHeight: 300,
+	          popoverOptions: CameraPopoverOptions,
+	          saveToPhotoAlbum: false,
+	          correctOrientation:true
+	        };
+	       
+	      $cordovaCamera.getPicture(options).then(function (imageData) {
+	            $scope.pic = "data:image/jpeg;base64," + imageData;
+	            $scope.my.image = "data:image/jpeg;base64," + imageData;
+	        }, function (err) {
+	            alert('can not load image');
+	      });
+	}
 });
