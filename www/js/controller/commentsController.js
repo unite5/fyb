e408e991d7f6d86a44c4d5082577288793dfb24c
Jitendra Patwa <jitendrapatwa135@gmail.com>
2017-03-29 23:13:35 +0530

    first

--- www/js/controller/commentsController.js
+++ www/js/controller/commentsController.js
@@ -1,10 +1,10 @@
 //angular.module('besties')
-besties.controller('commentsController',function($scope){
+besties.controller('commentsController',function($scope,$cordovaInAppBrowser){
 	        /**
         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
         
-        var disqus_config = function () {
+        /*var disqus_config = function () {
         this.page.url = 'http://test.dr-ambedkar.in/mobile/app/disqus/comments';  // Replace PAGE_URL with your page's canonical URL variable
         this.page.identifier = 'besties'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
@@ -14,5 +14,21 @@ besties.controller('commentsController',function($scope){
         s.src = 'https://jpbesties.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
-        })();
+        })();*/
+        var url = localStorage.myURL+"/mobile/app/disqus/comments";
+        var defaultOptions = {
+            location: 'no',
+            clearcache: 'no',
+            toolbar: 'no'
+        };
+        document.addEventListener("deviceready", function () {
+            $cordovaInAppBrowser.open(url, '_blank', defaultOptions)
+              .then(function(event) {
+                console.log(event);
+              })
+              .catch(function(event) {
+                console.error(event);
+              });
+            $cordovaInAppBrowser.close();
+        }, false);
 });
